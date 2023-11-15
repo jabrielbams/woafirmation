0ec24f09989280761ee7b4bb10e9e1c52c905250
mahesabagusr <mahesabagusraditya1@gmail.com>
2023-11-15 15:10:21 +0700

    bugfix - API Fetch

--- src/app/components/quote.js
+++ src/app/components/quote.js
@@ -1,47 +1,51 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../page.module.css";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
-export default function Quotes({ token, signature }) {
-  // const token = token
-  // const signature = signature
-  const [data, setData] = useState();
+const Quotes = ({ token, signature }) => {
+  const [data, setData] = useState(null);
 
-  console.log(token, signature);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          `https://encouraging-bear-gaiters.cyclic.app/getQuotes`,
-          {
-            params: {
-              token: token,
-              signature: signature,
-            },
-          }
+          `https://encouraging-bear-gaiters.cyclic.app/getQuotes/${token}/${signature}`
         );
-        // const result = await response.json()
-        console.log("response" + result);
-        setData(response.data);
+        const result = response.data;
+        setData(result);
       } catch (err) {
-        console.log(err);
+        console.error(err);
       }
     };
-    fetchData();
-  }, [token, signature]);
+
+    fetchData(); // Call the fetchData function when the component mounts
+
+    // If you need to clean up, you can return a cleanup function from useEffect
+    return () => {
+      // Cleanup logic (if needed)
+    };
+  }, [token, signature]); // Dependency array to re-run effect when token or signature changes
 
   return (
     <div className={styles.content}>
-      <div className={styles.leftContent}>
-        <h1 className={styles.idContent}>01</h1>
-      </div>
-      <div className={styles.rightContent}>
-        <p className={styles.textContent}>{data.quotes}</p>
-        <p className={styles.author}>{data.author}</p>
-      </div>
+      {data ? (
+        <>
+          <div className={styles.leftContent}>
+            <h1 className={styles.idContent}>01</h1>
+          </div>
+          <div className={styles.rightContent}>
+            <p className={styles.textContent}>{data.quotes}</p>
+            <p className={styles.author}>{data.author}</p>
+          </div>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
-}
+};
+
+export default Quotes;
--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,47 @@
 "use client";
 // Import library yang diperlukan
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  useParams,
-  Routes,
-} from "react-router-dom";
+import { useEffect } from 'react';
+import Image from "next/image";
+import styles from "./page.module.css";
+import Quotes from "./components/quote";
+import { useParams } from "react-router-dom";
+import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/router';
 
 import Page from "./screens/main";
 // Komponen utama yang menggunakan React Router
 const Home = () => {
+  const searchParams = useSearchParams()
+  const token = searchParams.get('token')
+  const signature = searchParams.get('signature')
   return (
-    <Router>
-      <Routes>
-        <Route
-          path='/getquote'
-          Component={Page}
-        />
-      </Routes>
-    </Router>
+    <main className={styles.main}>
+      <div className={styles.header}>
+        <div className={styles.leftHeader}>
+          <p className={styles.leftText}>Quotes for you</p>
+          <p className={styles.leftDate}>Today - Friday</p>
+        </div>
+        <div className={styles.rightHeader}>
+          <Image
+            src='/woa.svg'
+            alt='WOA Logo'
+            width={100}
+            height={24}
+            priority
+          />
+        </div>
+      </div>
+      <div className={styles.divider}></div>
+
+      <div className={styles.body}>
+        <Quotes token={token} signature={signature} />
+      </div>
+
+      <div className={styles.graphic}>
+        <div className={styles.box} />
+        <div className={styles.ellipse} />
+      </div>
+    </main>
   );
 };
 
